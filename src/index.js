b48beb04d9dc72981cbf6d7a1d3268bad648da96
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-03-01 20:20:20 +0200

    problem with languages

--- src/index.js
+++ src/index.js
@@ -16,35 +16,47 @@ inputEl.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
 function onInputCountry(event) {
   seachQuery = event.target.value.trim();
 
-  let arrayOfCountry = fetchCountries(seachQuery)
-    .then(res => console.log(res.length))
-    .catch(error => console.log(error));
-
-  // console.log((Object.keys(arrayOfCountry)).length);
-}
-
-if (arrayOfCountry.length > 10) {
-  Notiflix.Notify.info(
-    'Too many matches found. Please enter a more specific name.'
-  );
-  return;
-}
-
-if (arrayOfCountry.length >= 2 && seachQuery.length <= 10) {
   fetchCountries(seachQuery)
-    .then(renderCountrisName)
+    .then(res => {
+      console.log(res);
+      if (res.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (res.length >= 2 && seachQuery.length <= 10) {
+        renderCountrisName(res);
+      }
+      if (res.length === 1) {
+        renderCountrisName(res);
+        renderCountryInfo(res);
+      }
+    })
     .catch(error => console.log(error));
-
-  function renderCountrisName(seachQuery) {
-    const markup = seachQuery
-      .map(({ name, flags }) => {
-        return `<li class = "country-item"><img class = "country-img" src="${flags.svg}" alt="${flags.alt}"><span>${name.common}</span></li>`;
-      })
-      .join('');
-    countryListEl.innerHTML = markup;
-  }
 }
 
-//   // if ((arrayOfCountry.length = 1)) {
-//   // }
-// }
+function renderCountrisName(seachQuery) {
+  const markup = seachQuery
+    .map(({ name, flags }) => {
+      return `<li class = "country-item"><img class = "country-img" src="${flags.svg}" alt="${flags.alt}"><span>${name.common}</span></li>`;
+    })
+    .join('');
+  countryListEl.innerHTML = markup;
+}
+
+function renderCountryInfo(country) {
+  const markupCountry = country
+    .map(({ capital, population, languages }) => {
+      
+      return `<p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <ul class="list-languages">
+      {{#each languages}}
+      <li>{{language}}</li>
+      {{/each}}
+      </ul>`
+    })
+    .join('');
+  countryInfoEl.innerHTML = markupCountry;
+}

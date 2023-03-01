04279e6117535639a4305ca4460aabb1e3e8bf6c
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-03-01 17:36:52 +0200

    add function

--- src/index.js
+++ src/index.js
@@ -17,33 +17,34 @@ function onInputCountry(event) {
   seachQuery = event.target.value.trim();
 
   let arrayOfCountry = fetchCountries(seachQuery)
-    .then(res => console.log(res))
+    .then(res => console.log(res.length))
     .catch(error => console.log(error));
 
-  console.log(typeof(arrayOfCountry));
+  // console.log((Object.keys(arrayOfCountry)).length);
 }
-//   if (result.length > 10) {
-//     Notiflix.Notify.info(
-//       'Too many matches found. Please enter a more specific name.'
-//     );
-//     return;
-//   }
 
-//   if (result.length >= 2 && seachQuery.length <= 10) {
-//     fetchCountries(seachQuery)
-//       .then(renderCountrisName)
-//       .catch(error => console.log(error));
-//   }
+if (arrayOfCountry.length > 10) {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+  return;
+}
 
-//   // if ((seachQuery.length = 1)) {
+if (arrayOfCountry.length >= 2 && seachQuery.length <= 10) {
+  fetchCountries(seachQuery)
+    .then(renderCountrisName)
+    .catch(error => console.log(error));
+
+  function renderCountrisName(seachQuery) {
+    const markup = seachQuery
+      .map(({ name, flags }) => {
+        return `<li class = "country-item"><img class = "country-img" src="${flags.svg}" alt="${flags.alt}"><span>${name.common}</span></li>`;
+      })
+      .join('');
+    countryListEl.innerHTML = markup;
+  }
+}
+
+//   // if ((arrayOfCountry.length = 1)) {
 //   // }
-// }
-
-// function renderCountrisName(seachQuery) {
-//   const markup = seachQuery
-//     .map(({ name, flags }) => {
-//       return `<li class = "country-item"><img class = "country-img" src="${flags.svg}" alt="${flags.alt}"><span>${name.common}</span></li>`;
-//     })
-//     .join('');
-//   countryListEl.innerHTML = markup;
 // }
